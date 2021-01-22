068faa0ab90638a18bbae72b8f26387fa9ef2c11
Ryuzoutamura <ryuzouqqq@hotmail.co.jp>
2021-01-22 23:41:52 +0000

    [Add] トップページのひな形を作成

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,26 @@
+class BooksController < ApplicationController
+  def top
+  end
+
+  def index
+  end
+  
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+  
+end
