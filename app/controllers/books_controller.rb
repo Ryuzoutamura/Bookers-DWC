73d275f533c383240dbf518fa24c7357f32c8629
Ryuzoutamura <ryuzouqqq@hotmail.co.jp>
2021-02-22 08:49:33 +0000

    [Finish] 完成、課題提出一回目

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,16 +4,18 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
-  end
-  
-  def new
     @book = Book.new
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+        flash[:notice] = "Book was successfully created."
+        redirect_to book_path(@book.id)
+    else
+        @books = Book.all.order(created_at: :desc)
+        render :index
+    end
   end
 
   def show
@@ -25,9 +27,13 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book)
+    else
+      render :edit
+    end
   end
   
   def destroy
@@ -38,7 +44,7 @@ class BooksController < ApplicationController
   
   private
   def book_params
-    params.permit(:title, :body)
+    params.require(:book).permit(:title, :body)
   end
   
 end
